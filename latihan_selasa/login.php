9cdd3687a20a0bdf6850fb75dc6e709ec7c222eb
tafrij <mamunamri97@gmail.com>
2019-01-24 20:40:32 +0700

    git push * origin master

---
+++ latihan_selasa/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>login</title>
+  </head>
+  <body>
+    <form action="" method="post">
+      <label for="">username</label> <br>
+      <input type="text" name="username" ><br><br>
+
+      <label for="">nama</label> <br>
+      <input type="text" name="username" value=""><br><br>
+
+      <label for="">password</label> <br>
+      <input type="password" name="password" ><br><br>
+    </form>
+  </body>
+</html>
