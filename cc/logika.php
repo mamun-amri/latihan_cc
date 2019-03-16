15b3b016abaaa0d512494971c74d98e8a4c47718
tafrij <mamunamri97@gmail.com>
2019-03-16 10:06:19 +0700

    git push origin master

--- cc/logika.php
+++ cc/logika.php
@@ -17,9 +17,33 @@
     echo "<br>";
   }
   echo "<h1>latihan</h1>";
-  for($i=5;$i<=5;$i--){
+  for($i=5;$i>=1;$i--){
     for($a=1;$a<=$i;$a++){
       echo '*';
+    }
+    echo "<br>";
+  }
+  echo "<h1>latihan</h1>";
+  for($i=9;$i>=1;$i--){
+    for($a=2;$a<=$i;$a++){
+      echo '1';
+    }
+    echo $i."<br>";
+  }
+  echo "<h1>latihan</h1>";
+  for($i=1;$i<=5;$i++){
+    for($a=1;$a<=5;$a++){
+      for($b=1;$b<=$i;$b++){
+        echo $b;
+      }
+    }
+    echo "<br>";
+  }
+  for($i=6;$i>=1;$i--){
+    for($a=1;$a<=5;$a++){
+      for($b=1;$b<=$i;$b++){
+        echo $b;
+      }
     }
     echo "<br>";
   }
